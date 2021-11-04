6746f4cb97f95483e9d5de13e048bf761ee1cb10
davidkharbin <david_k_harbin@hotmail.com>
2021-11-04 08:45:46 -0600

    feat: favicon added, SEO - site title edit

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ const guid = process.env.NETLIFY_GOOGLE_ANALYTICS_ID;
 
 module.exports = {
   siteMetadata: {
-    title: 'Test',
+    title: 'Seahorse Safe',
     description: 'my theme'
   },
   plugins: [
